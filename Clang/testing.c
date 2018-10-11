752c9b9597a4c6ca022a610d7d0fc607fb11a04a
bgoncharuck <dariydragon26@gmail.com>
2018-10-11 12:19:35 +0300

    bbia_shift_right debug

--- Clang/testing.c
+++ Clang/testing.c
@@ -22,6 +22,15 @@ int main (int argc, char * argv[]) {
 	bbia_bitshift_left (firstTest, 2);
 	bbia_print_levelValue (firstTest);
 
+	for (int lvl = BBIA_LEVEL_TOP; lvl >= 0; lvl--)
+		bbia_at_set (firstTest, lvl, (stuaa_bitflag(1) | stuaa_bitflag(2)
+	| stuaa_bitflag(3) | stuaa_bitflag(4) ));
+	bbia_print_levelValue (firstTest);
+
+	bbia_bitshift_right (firstTest, 3);
+	// bbia_print_levelValue (firstTest);
+
+
 	bbia_free (firstTest);
 
 	return EXIT_SUCCESS;
