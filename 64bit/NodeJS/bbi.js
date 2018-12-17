8af3cc63963e57e5ecfb4e829678b0a10b93ea86
bgoncharuck <dariydragon26@gmail.com>
2018-12-17 21:17:24 +0200

    constructor created

--- 64bit/NodeJS/bbi.js
+++ 64bit/NodeJS/bbi.js
@@ -1,23 +1,37 @@
 const uint = require('./ieee754_asInt.js');
 const constants = require('./bbi_constants');
 
-function bbi () {
-	this.at = new Array(constants.LEVEL_COUNT).fill(0);
-	this.sign = false;
-	this.lvlButton = constants.LEVEL_TOP;
+
+
+class bbi {
+	constructor () {
+		if (arguments.length == 2 && arguments[0] instanceof Boolean && arguments[1] instanceof Number) {
+			this.at = new Array(constants.LEVEL_COUNT).fill(0);
+			this.at[constants.LEVEL_TOP] = arguments[1];
+			this.sign = arguments[0];
+			this.lvlButton = constants.LEVEL_TOP;
+		}
+		else if (arguments.length && arguments[0] instanceof bbi) {
+			this.at = arguments[0].at.slice(0);
+			this.sign = arguments[0].sign;
+			this.lvlButton = arguments[0].lvlButton;
+		}
+		else {
+			this.at = new Array(constants.LEVEL_COUNT).fill(0);
+			this.sign = false;
+			this.lvlButton = constants.LEVEL_TOP;
+		}
+	}
+
 }
 
-// from system integer
-function bbi (sign, integer) {
-	this.at = new Array(constants.LEVEL_COUNT).fill(0);
-	this.at[constants.LEVEL_TOP] = integer;
-	this.sign = sign;
-	this.lvlButton = constants.LEVEL_TOP;
-}
+let bbiConstructorTest_1 = new bbi();
+let bbiConstructorTest_2 = new bbi(false, 7);
+let bbiConstructorTest_3 = new bbi(bbiConstructorTest_2);
+console.log(bbiConstructorTest_1 instanceof bbi);
+console.log(bbiConstructorTest_2 instanceof bbi);
+console.log(bbiConstructorTest_3 instanceof bbi);
 
-// copy
-function bbi (from) {
-	this.at = from.at.slice(0);
-	this.sign = from.sign;
-	this.lvlButton = from.lvlButton;
-}
+module.exports = {
+
+};
