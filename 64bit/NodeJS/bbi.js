76403c59b81499414b8d0d4cd6a745f7948bb8c6
bgoncharuck <dariydragon26@gmail.com>
2018-12-17 21:22:27 +0200

    class is portable

--- 64bit/NodeJS/bbi.js
+++ 64bit/NodeJS/bbi.js
@@ -1,8 +1,6 @@
 const uint = require('./ieee754_asInt.js');
 const constants = require('./bbi_constants');
 
-
-
 class bbi {
 	constructor () {
 		if (arguments.length == 2 && arguments[0] instanceof Boolean && arguments[1] instanceof Number) {
@@ -25,13 +23,6 @@ class bbi {
 
 }
 
-let bbiConstructorTest_1 = new bbi();
-let bbiConstructorTest_2 = new bbi(false, 7);
-let bbiConstructorTest_3 = new bbi(bbiConstructorTest_2);
-console.log(bbiConstructorTest_1 instanceof bbi);
-console.log(bbiConstructorTest_2 instanceof bbi);
-console.log(bbiConstructorTest_3 instanceof bbi);
-
 module.exports = {
-
-};
+	bbi
+}
--- 64bit/NodeJS/testing.js
+++ 64bit/NodeJS/testing.js
@@ -1,9 +1,16 @@
 const uint = require('./ieee754_asInt.js');
 const constants = require('./bbi_constants.js');
-const bbi = require('./bbi.js');
+const {bbi} = require('./bbi.js');
 
 // let uintToBaseCheck = 0xFFFFFFFF;
 // let uintToBaseCheck = constants.LEVEL_IS_FULL;
 let uintToBaseCheck = 7;
 console.log(`${uint.toBaseOfTwo(uintToBaseCheck,2)}`);
 console.log(`${uint.toBaseOfTwo(uintToBaseCheck,16)}`);
+
+let bbiConstructorTest_1 = new bbi();
+let bbiConstructorTest_2 = new bbi(false, 7);
+let bbiConstructorTest_3 = new bbi(bbiConstructorTest_2);
+console.log(bbiConstructorTest_1 instanceof bbi);
+console.log(bbiConstructorTest_2 instanceof bbi);
+console.log(bbiConstructorTest_3 instanceof bbi);
