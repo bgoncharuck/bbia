47c8261d405f34b22cfe81fb0344af2522f30208
bgoncharuck <dariydragon26@gmail.com>
2018-12-17 20:23:24 +0200

    constants

--- 64bit/NodeJS/bbi.js
+++ 64bit/NodeJS/bbi.js
@@ -1,2 +1,6 @@
 const uint = require('./ieee754_asInt.js');
 const constants = require('./bbi_constants');
+
+function bbi () {
+	this.at = new Array(constants.LEVEL_COUNT).fill(0);
+}
--- 64bit/NodeJS/testing.js
+++ 64bit/NodeJS/testing.js
@@ -1,8 +1,9 @@
 const uint = require('./ieee754_asInt.js');
 const constants = require('./bbi_constants.js');
-const bbi = require('./bbi.js');
+// const bbi = require('./bbi.js');
 
 // let uintToBaseCheck = 0xFFFFFFFF;
+// let uintToBaseCheck = constants.LEVEL_IS_FULL;
 let uintToBaseCheck = 7;
 console.log(`${uint.toBaseOfTwo(uintToBaseCheck,2)}`);
 console.log(`${uint.toBaseOfTwo(uintToBaseCheck,16)}`);
